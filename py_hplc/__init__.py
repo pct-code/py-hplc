773fa8f60c5ba30e14c7e6caa57d628a2041e097
teauxfu <alexmw777@gmail.com>
2021-04-13 20:14:09 -0500

    fix imports

--- py_hplc/__init__.py
+++ py_hplc/__init__.py
@@ -1,4 +1,4 @@
 """An unoffical Python wrapper for the SSI-Teledyne Next Generation class HPLC pumps."""
 
-from .next_gen_pump import NextGenPump
-from .next_gen_pump_base import NextGenPumpBase
+from py_hplc.next_gen_pump import NextGenPump
+from py_hplc.next_gen_pump_base import NextGenPumpBase

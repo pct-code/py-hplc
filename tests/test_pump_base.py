55136030d3e485ffaa146b6dfd12f1f078f3b5b3
Alex Whittington <alexmw777@gmail.com>
2021-05-05 23:31:24 -0500

    linting

--- tests/test_pump_base.py
+++ tests/test_pump_base.py
@@ -1,10 +1,11 @@
-from py_hplc.pump_error import PumpError
 import sys
 import unittest
 
 from serial.serialutil import SerialException
 
 from py_hplc.pump_base import NextGenPumpBase
+from py_hplc.pump_error import PumpError
+
 
 class TestPumpBase(unittest.TestCase):
     SERIAL_PORT = "COM3"
@@ -15,7 +16,7 @@ class TestPumpBase(unittest.TestCase):
         try:
             TestPumpBase.PUMP = NextGenPumpBase(self.SERIAL_PORT)
         except SerialException:
-            self.fail(f'No such port {self.SERIAL_PORT}')
+            self.fail(f"No such port {self.SERIAL_PORT}")
 
     def test_identify(self) -> None:
         """Tests initializing a NextGenPumpBase,
@@ -28,17 +29,17 @@ class TestPumpBase(unittest.TestCase):
         self.assertIsNotNone(pump.version)
         self.assertIsNotNone(pump.head)
         self.assertIsNotNone(pump.flowrate_factor)
-    
+
     def test_write_read(self) -> None:
         pump = TestPumpBase.PUMP
         # redundant assertion that we can read/write
-        self.assertIn('Version', pump.write('id'))
+        self.assertIn("Version", pump.write("id"))
 
     def test_command(self) -> None:
         # make sure bad commands throw an error
         pump = TestPumpBase.PUMP
         self.assertRaises(PumpError, pump.command, "foobar")
-    
+
     def test_close(self) -> None:
         pump = TestPumpBase.PUMP
         pump.close()
